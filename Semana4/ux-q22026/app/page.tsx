003024e7caa9503f5912e4afda7ff76956ee1a65
Claudia Cortes <claudiacortes@Claudias-MacBook-Pro.local>
2026-05-18 20:32:42 -0600

    clase 18 de mayo: hooks

--- Semana4/ux-q22026/app/page.tsx
+++ Semana4/ux-q22026/app/page.tsx
@@ -1,18 +1,37 @@
+'use client';
+import { useState } from "react";
+
 import Boton from "./components/boton";
 
 export default function Home() {
+  const [isLoggedIn,setIsLoggedIn] = useState<boolean>(false);
+  const [password,setPassword] = useState<string>("");
+  const modificarValor = ()=>{
+    setIsLoggedIn(!isLoggedIn);
+}
+
+const validar = ()=>{
+  if(password.length<=8){
+    console.log('La contrasena debe tener 8 caracteres')
+  }
+}
   return (
     <div 
     className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"
     >
+      <h1>{password}</h1>
+
+      <input type="text" value={password} onChange={(event)=>{setPassword(event.currentTarget.value)}}/>
+      
       {
-        Boton()
+        isLoggedIn ? <h1>Hola fulano</h1> : <h1>Adiosin</h1>
       }
-      <Boton/>
-      <Boton/>
-      <Boton/>
-      <Boton/>
-      <Boton/>
+
+      <button onClick={()=>{
+    setIsLoggedIn(!isLoggedIn);
+    console.log('click!!!!');
+}}> {isLoggedIn ? 'log out': 'log in'}</button>
+      
       
 
     </div>
