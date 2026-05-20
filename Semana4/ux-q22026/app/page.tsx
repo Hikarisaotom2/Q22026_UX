3c2978f1f5ececda956ffb27e4be80b26954df79
Claudia Cortes <claudiacortes@Claudias-MacBook-Pro.local>
2026-05-19 20:08:08 -0600

    Clase 19 de mayo: use effect

--- Semana4/ux-q22026/app/page.tsx
+++ Semana4/ux-q22026/app/page.tsx
@@ -1,14 +1,51 @@
 'use client';
-import { useState } from "react";
-
-import Boton from "./components/boton";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [isLoggedIn,setIsLoggedIn] = useState<boolean>(false);
+  const [errores,setErrores] = useState<string[]>([]);
   const [password,setPassword] = useState<string>("");
-  const modificarValor = ()=>{
-    setIsLoggedIn(!isLoggedIn);
+
+
+  // useEffect(/*callback*/,/*dependencias*/);
+
+
+  const validarContrasena =  ()=>{
+   const nuevoErrores = [];
+    if(password.length<8){
+      nuevoErrores.push('La contraseña debe tener almenos 8 caracteres');
+   }
+
+  if(!password.includes("!") ){
+      nuevoErrores.push('La contraseña debe tener un caracter especial caracteres');
+   }
+
+   if(!password.includes("0")){
+      nuevoErrores.push('La contraseña debe tener un caracter especial caracteres');
+   }
+
+    setErrores(nuevoErrores);
 }
+
+
+// use effect con una dependencia.
+  useEffect(()=>{
+    console.log("ejecutando useeffect....")
+      validarContrasena();
+  },[password]);
+
+  // useeffect reacionando al primer render. 
+  useEffect(()=>{
+    //set loading true...
+      console.log('primer render')
+      //peticion a la BD.....
+    // set loading false
+  }, []);
+
+  // useeffect que reacciona. todos los renders. .......
+  useEffect(()=>{
+   console.log('se detecto un render....') 
+  });
 
 const validar = ()=>{
   if(password.length<=8){
@@ -20,8 +57,12 @@ const validar = ()=>{
     className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"
     >
       <h1>{password}</h1>
-
-      <input type="text" value={password} onChange={(event)=>{setPassword(event.currentTarget.value)}}/>
+      {
+        errores.map((it, key)=>{
+          return <h2 key={key}>{it}</h2>
+        })
+      }
+      <input type="text" value={password} onChange={(event)=>{setPassword(event.currentTarget.value) }}/>
       
       {
         isLoggedIn ? <h1>Hola fulano</h1> : <h1>Adiosin</h1>
